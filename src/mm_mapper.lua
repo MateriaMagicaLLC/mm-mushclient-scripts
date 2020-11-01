374606555bf0dab13a2cb0ae4c4192f0b6ecb4a3
MueRTe <mu3r73@yandex.com>
2020-10-31 21:20:10 -0300

    fix: trying to index tags/flags when room isn't cached

--- src/mm_mapper.lua
+++ src/mm_mapper.lua
@@ -811,13 +811,13 @@ end
 
 
 local function is_room_tagged_as(uid, tag)
-  local tags = rooms[uid].tags or ""
+  local tags = get_room(uid).tags or ""
   return string.find(tags or "", tag)
 end
 
 
 local function is_room_flagged_as(uid, flag)
-  local flags = rooms[uid].flags or ""
+  local flags = get_room(uid).flags or ""
   return string.find(flags or "", flag)
 end
 
